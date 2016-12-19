7a6537662439ea5c4e6f78e787ec3040ec592614
Etudiant <Etudiant@L35-19>
2016-12-19 20:56:31 +0100

    2st commit (en local évidemment)

--- vues/menu-header-footer/menu.php
+++ vues/menu-header-footer/menu.php
@@ -1,8 +1,8 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <ul class="nav navbar-nav">
+            <li><a href="index.php?navigation=presentation">Présentation</a></li>
+            <li><a href="index.php?navigation=liste_cours">Liste des cours</a></li>
+        </ul>
+    </div>
+</nav>
