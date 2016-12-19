13c83a5992da97e2c197b72b8dd4bba2557bf01d
Etudiant <Etudiant@L35-19>
2016-12-19 20:58:28 +0100

    2st commit (en local évidemment)

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo'texte ';
+        include('./vues/vue_presentation.php');
         ?>
     </body>
 </html>
